f50830cf78e0df8a3f8b7d9b936cd41185133727
Ky So <klimochkinaff@mail.ru>
2022-03-04 11:00:11 +0300

    создала класс для подгрузки данных

---
+++ Demon/Class/LoadList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demon
+{
+    class LoadList
+    {
+        public List<Service> service;
+        public List<Client> clients;
+    }
+}
